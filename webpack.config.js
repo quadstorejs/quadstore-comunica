48f47f54b2c31564850412115018584225e2cf66
Jacopo Scazzosi <jacopo@scazzosi.com>
2021-04-07 13:09:22 +0200

    v1.1.0-beta.0, reverted to having sparqljs, sparqlalgebrajs as peer deps, moved @comunica/types to a standard dep

--- webpack.config.js
+++ webpack.config.js
@@ -118,6 +118,8 @@ const replacementExternals = {
 const peerDependenciesExternals = [
   'asynciterator',
   'rdf-data-factory',
+  'sparqlalgebrajs',
+  'sparqljs',
 ].reduce((acc, moduleName) => {
   acc[moduleName] = `commonjs2 ${moduleName}`;
   return acc;
